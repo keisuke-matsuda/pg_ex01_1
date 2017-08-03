141be264e6c54191867710c7f9cadb5dad04c0ec
user <user@user-PC>
2017-08-03 10:47:48 +0900

    微修正

--- src/pg_ex01_1/FigureAndInteger.java
+++ src/pg_ex01_1/FigureAndInteger.java
@@ -11,14 +11,14 @@ public class FigureAndInteger {
 			// ①[4で割り切れる]   	かつ [3で割り切れる]	 	→ '◆'を出力
 			// ②[4で割り切れる] 	かつ [3で割り切れない]     	→ '□'を出力
 			// ③[4で割り切れない] 	かつ [3で割り切れる]	   	→ '△'を出力
-			// ④上記に該当しない								→ 数字を出力
+			// ④上記に該当しない							→ 数字を出力
 			if ( n % 4 == 0 && n % 3 == 0 ){			//条件分岐①
 				System.out.println('◆');
 			}else if ( n % 4 == 0 && n % 3 != 0 ){		//条件分岐②
 				System.out.println('□');
 			}else if ( n % 4 != 0 && n % 3 == 0 ){		//条件分岐③
 				System.out.println('△');
-			}else{										//条件分岐④
+			}else{									//条件分岐④
 				System.out.println(n);
 			}
 
